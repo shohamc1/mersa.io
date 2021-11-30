1f34c86f8ea43c7181c550cc1c27523d761dd1c1
shohamc1 <shhmchk@gmail.com>
2021-11-30 15:09:34 +0800

    add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,41 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React, { useEffect } from "react";
+import { Link, navigate } from "gatsby";
+import { Helmet } from "react-helmet";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import Header from "../components/Header";
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
+  useEffect(() => {
+    setTimeout(function () {
+      navigate("/");
+    }, 5000);
+  }, []);
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <div class="flex w-full h-full justify-center bg-backgroundGray">
+      <Helmet>
+        <title>Not Found | mersa.io</title>
+        <meta name="theme-color" content="#143A61"></meta>
+      </Helmet>
+      <div class="absolute top-0 left-0 w-full h-full opacity-30 bg-gradient-to-b from-landingBlue to-backgroundGray"></div>
+      <main class="h-screen w-full px-4 lg:px-0 lg:w-3/4 z-10">
+        <Header />
+        <div class="flex flex-col justify-center items-center text-center mb-auto">
+          <span class="text-5xl lg:text-7xl font-header tracking-wider text-white">
+            Content not found
+          </span>
+          <Link
+            to="/"
+            class="font-subheader bg-white rounded-lg text-black text-2xl px-4 py-2 mt-2"
+          >
+            Go back home
+          </Link>
+          <span class="mt-4 text-white text-2xl font-thin">
+            You will be redirected to the home page in 5 seconds.
+          </span>
+        </div>
+      </main>
+    </div>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
